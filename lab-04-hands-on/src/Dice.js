066f4ca475307b2d26bf450c1a795c683e2e98ad
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-06-27 08:59:31 +0000

    Added hands on for lab 4

--- lab-04-hands-on/src/Dice.js
+++ lab-04-hands-on/src/Dice.js
@@ -2,14 +2,21 @@
 import {Component} from 'react';
 
 export default class Dice extends Component {
+
+    // Methods
+    getRandomNumber() {
+        return Math.floor(Math.random() * 6 + 1)
+    }
+
     // state variables
     state = {
-        'value': 1
+        // Set the initial value of number to be a random number from 1 to 6
+        'value': this.getRandomNumber()
     }
 
     rollDice = () => {
         this.setState({
-            'value': Math.floor(Math.random() * 6 + 1)
+            'value': this.getRandomNumber()
         })
     }
 
@@ -23,6 +30,8 @@ export default class Dice extends Component {
         return 'black';
     }
 
+    // Never call setState inside render() function -> will create an infinite rendering issue
+    // -> instead call it via an event listener
     render() {
         return (
             <div style={{
